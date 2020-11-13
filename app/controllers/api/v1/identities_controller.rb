215e2f46597aa59cf74cf5b62435b537abf0831c
matt_kuras <mattkuras@gmail.com>
2020-11-13 16:46:20 -0500

    puts in create id method

--- app/controllers/api/v1/identities_controller.rb
+++ app/controllers/api/v1/identities_controller.rb
@@ -14,6 +14,7 @@ module Api
             end
 
             def create 
+                puts current_user
                 identity = current_user.identities.build(identity_params)
                 if identity.save
                     render json: IdentitySerializer.new(identity).serialized_json
