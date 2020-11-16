5aff9a12085acdc510bd27214c332179016ff6df
matt_kuras <mattkuras@gmail.com>
2020-11-16 14:25:13 -0500

    debugging

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,4 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_habit-helper', domain: 'localhost:3000'
-  else
+    Rails.application.config.session_store :cookie_store, key: '_habit-helper', domain: :all
     Rails.application.config.session_store :cookie_store, key: '_habit-helper' 
   end
